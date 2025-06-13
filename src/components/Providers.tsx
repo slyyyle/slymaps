767ccafbeab058ba9eaa24d3ed000aaee92328a3
slyle <khammitt1@gmail.com>
2025-06-13 14:21:43 -0500

    fix: move 'use client' directive to top of Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 'use client';
+import React from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/components/theme-provider';
